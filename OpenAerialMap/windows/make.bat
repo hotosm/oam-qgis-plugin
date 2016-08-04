2b4d4e2777c398ce3fcb45790c018cc8d273d995
yojiyojiyoji <yoji.salut@gmail.com>
2016-08-03 23:39:18 -0400

    modified make.bat file

--- OpenAerialMap/windows/make.bat
+++ OpenAerialMap/windows/make.bat
@@ -2,8 +2,8 @@
 REM Please setup the path to pyrcc4.exe before executing this batch file.
 REM The file should be found in <Your QGIS Install Directory>\bin\pyrcc4.exe
 REM Ex. For Essen (version 2.14.4),
-REM C:\Program Files\QGIS Essen\bin
-REM If using Essen, You can execute the pyrcc4_set_path.bat file from command prompt.
+REM SET PATH=%PATH%;C:\Program Files\QGIS Essen\bin or C:\Program Files\QGIS 2.14.4\bin, etc.
+
 
 REM Set the default plugin folder
 SET PLUGIN_NAME=OpenAerialMap
@@ -13,14 +13,16 @@ SET FOLDERS=ext_libs gui icon module temp i18n
 SET TARGET_FILES=__init__.py ABOUT INSTALL LICENSE metadata.txt oam_main.py^
                 oam-qgis-plugin.conf.sample resources.qrc resources_rc.py
 
-REM Create help, and translation files
-REM SET HELPFILES=help
+REM Set the build folder for help files
+SET HELP=help\build\html
+
+REM set the resource folder for translation
 REM SET LOCALS=, LRELEASE=, etc.
 
-ECHO Create resources_rc.py
-pyrcc4 -o ../resources_rc.py ../resources.qrc
-
 if "%1" == "deploy" (
+  ECHO Create resources_rc.py
+  pyrcc4 -o ..\resources_rc.py ..\resources.qrc
+
   ECHO Create OAM Plugin Directory %OAM_PLUGIN_DIR%
   mkdir %OAM_PLUGIN_DIR%
 
@@ -28,16 +30,23 @@ if "%1" == "deploy" (
     xcopy ..\%%f %OAM_PLUGIN_DIR%\%%f\ /E /Y
   )
 
+  REM xcopy ..\%HELP% %OAM_PLUGIN_DIR%\help\ /E /Y
+
   FOR %%f IN (%TARGET_FILES%) DO (
     copy ..\%%f %OAM_PLUGIN_DIR%\%%f
   )
-  
+
   ECHO Delete resources_rc.py file
   del ..\resources_rc.py
 )
 if "%1" == "derase" (
-	echo under construction - derase
+	ECHO under construction - derase
 )
 if "%1" == "doc" (
-	echo under construction - doc
+	ECHO Building help file in html format...
+  REM CALL ..\help\make.bat html
+)
+if "%1" == "docclean" (
+	ECHO Deleting help file...
+  REM del ..\help\build /S /F /Q
 )
--- OpenAerialMap/windows/pyrcc4_set_path.bat
+++ OpenAerialMap/windows/pyrcc4_set_path.bat
@@ -1,2 +1,2 @@
 REM Setup the path to pyrcc4.exe.
-SET PATH=%PATH%;C:\Program Files\QGIS Essen\bin
+SET PATH=%PATH%;C:\Program Files\QGIS %1\bin
