942dbeefb53f7a8f9cd3bd0e00e1602ed9d8954b
yojiyojiyoji <yoji.salut@gmail.com>
2016-11-27 23:07:39 -0500

    disabled the display of integrated image browser for temporal purpose

--- OpenAerialMap/oam_main.py
+++ OpenAerialMap/oam_main.py
@@ -41,7 +41,7 @@ import resources_rc
 from gui.img_uploader_wizard import ImgUploaderWizard
 from gui.img_search_dialog import ImgSearchDialog
 from gui.setting_dialog import SettingDialog
-from gui.integrated_img_browser import IntegratedImgBrowser
+# from gui.integrated_img_browser import IntegratedImgBrowser
 
 # set os-specific environment
 from set_env import SetEnvironment
@@ -209,9 +209,11 @@ class OpenAerialMap:
         webbrowser.open_new(url)
 
     def displayIntegratedImgBrowser(self):
-
+        pass
+        """
         self.integratedImgBrowser = IntegratedImgBrowser(self.iface)
         self.integratedImgBrowser.show()
+        """
 
     def initGui(self):
 
@@ -255,11 +257,13 @@ class OpenAerialMap:
             callback=self.displayHelp,
             parent=self.iface.mainWindow())
 
+        """
         self.add_action(
             icon_path_setting_dialog,
             text=self.tr(u'Search and Browse Imagery'),
             callback=self.displayIntegratedImgBrowser,
             parent=self.iface.mainWindow())
+        """
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
