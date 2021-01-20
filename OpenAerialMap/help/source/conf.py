8bc37c74385739ba20d75efc9aad53fdeda5ce89
koyo-jakanees <davince.koyo@gmail.com>
2021-01-20 20:01:07 +0300

    rename sphinx.ext.pngmath to sphinx.ext.imgmath to compile docs

--- OpenAerialMap/help/source/conf.py
+++ OpenAerialMap/help/source/conf.py
@@ -27,7 +27,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.todo',
-              'sphinx.ext.pngmath',
+              'sphinx.ext.imgmath',
               'sphinx.ext.viewcode',
               'sphinx.ext.autodoc']
 
